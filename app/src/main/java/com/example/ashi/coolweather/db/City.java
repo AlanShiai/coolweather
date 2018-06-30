885ce9304604255e1b36dd9996bde449b0c20e49
ashi <Aijun.Shi@windriver.com>
2018-06-30 09:47:27 +0800

    add database and table

---
+++ app/src/main/java/com/example/ashi/coolweather/db/City.java
@@ -0,0 +1,48 @@
+package com.example.ashi.coolweather.db;
+
+/**
+ * Created by ashi on 6/30/18.
+ */
+
+public class City {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
